56fc55955cb71a47ce959673449dbff56333930e
EC2 Default User <ec2-user@ip-172-31-14-90.ap-northeast-2.compute.internal>
2016-04-17 22:14:50 +0000

    done

--- Lsybot.java
+++ Lsybot.java
@@ -3,8 +3,19 @@
 class Lsybot {
 	public static void main(String[] args) throws Exception{
 		URLGetter getter = new URLGetter();
-		System.out.println("call getter");
-		System.out.println(getter.imgURLFetch(20));
+		final String directory = "Closer";
+		for(int i=1; i<=757; i++) {
+			System.out.print("getting URL for [" + i + "]... ");
+			String imageURL = getter.imgURLFetch(i);
+			System.out.print("saving [" + i + "]... ");
+			String zeropad;
+			if(i<10) zeropad = "00"+i;
+			else if(i<100) zeropad = "0"+i;
+			else zeropad = ""+i;
+			ImageGetter.saveImage(imageURL, directory+"/"+directory+zeropad+".jpg");
+		}
+		//System.out.println("call getter");
+		//System.out.println(getter.imgURLFetch(20));
 		//String imageURL = "http://dimg.donga.com/carriage/NEWS/content/SPORTS/Cartoon/2016/04/08/14600708491.jpg";
 		//ImageGetter.saveImage(imageURL, "tester757.jpg");
 	}
